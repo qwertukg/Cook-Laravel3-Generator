be350fe563f0f8ebe64a3cf74a4a147cec81e53a
Daniil <daniil.rakhmatulin@gmail.com>
2014-11-24 17:14:36 +0600

    adds some comments

--- Cook/Constructor/Storage.php
+++ Cook/Constructor/Storage.php
@@ -5,20 +5,25 @@ use Cook\Constructor;
 
 class Storage extends Fluent {
 
+	// Constructors result container.
 	public $constructors = array();
 
+	// Storage of basic constructos.
 	protected $storage = array();
 
+	// Storage of basic constructos.
 	protected $columns = array();
 
 	protected $currentBundle;
 
 	public function addBundle($bundle)
 	{
+		// Set the current bundle.
 		$this->currentBundle = $bundle;
 
 		if (!isset($this->storage[$bundle]))
 		{
+			// Add bundle to storage.
 			$this->storage[$bundle] = array();
 		}
 	}
@@ -27,6 +32,7 @@ class Storage extends Fluent {
 	{
 		if ($this->currentBundle) 
 		{
+			// Add constructor to storage with current bundle.
 			$this->storage[$this->currentBundle][$constructor->name][] = $constructor;
 		}
 	}
@@ -35,58 +41,78 @@ class Storage extends Fluent {
 	{
 		foreach ($this->storage as $bundleName => $tables) 
 		{
-			foreach ($tables as $tableName => $constructors) 
+			foreach ($tables as $constructorName => $constructors) 
 			{
-				if (!isset($this->columns[$tableName]))
+				if (!isset($this->columns[$constructorName]))
 				{
-					$this->columns[$tableName] = array();
+					// If curent constructor name is not in columns container, add this.
+					$this->columns[$constructorName] = array();
 				}
 
 				foreach ($constructors as $constructor) 
 				{
 					foreach ($constructor->commands as $command) 
 					{
+						// Determine columns, from constructor or command columns.
 						$columns = ($command->columns) ? $command->columns : $constructor->columns;
 
-						$this->{$command->type}($columns, $tableName);
+						// Run each command for current constructor and write result to columns container.
+						$this->{$command->type}($columns, $constructorName);
 					}
 
-					$constructor->bundle = $bundleName;
-					$constructor->columns = $this->columns[$constructor->name];
-					$this->constructors[$tableName] = $constructor;
-					
+					// Unset useless properties.
 					unset($constructor->commands, $constructor->connection, $constructor->engine);
+					
+					if (!isset($this->constructors[$constructorName])) 
+					{
+						// If this constructor not in constructors container, add result columns to this constructor.
+						$constructor->columns = $this->columns[$constructor->name];
+						$constructor->bundle = $bundleName;
+
+						// Add this constructor to constructors container.
+						$this->constructors[$constructorName] = $constructor;
+					}
+					else
+					{
+						// If this constructor in constructors container, change columns on this constructor.
+						$this->constructors[$constructorName]->columns = $this->columns[$constructor->name];
+						$this->constructors[$constructorName]->bundle = $bundleName;
+					}
 				}
 			}
 		}
 	}
 
-	protected function create($columns, $tableName)
+	protected function create($columns, $constructorName)
 	{
 		foreach ($columns as $column) 
 		{
-			$this->columns[$tableName][$column->name] = $column;
+			// Create new columns in columns container.
+			$this->columns[$constructorName][$column->name] = $column;
 		}
 	}
 
-	protected function add($columns, $tableName)
+	protected function add($columns, $constructorName)
 	{
 		foreach ($columns as $column) 
 		{
-			$this->columns[$tableName][$column->name] = $column;
+			// Add new columns to columns container.
+			$this->columns[$constructorName][$column->name] = $column;
 		}
 	}
 
-	protected function drop($columns, $tableName)
+	protected function drop($columns, $constructorName)
 	{
-		$this->columns[$tableName] = array();
+		// Clear current columns container.
+		$this->columns[$constructorName] = array();
 	}
 
-	protected function drop_column($columns, $tableName)
+	protected function drop_column($columns, $constructorName)
 	{
 		foreach ($columns as $column) 
 		{
-			unset($this->columns[$tableName][$column]);
+			// Removes columns from columns container.
+			unset($this->columns[$constructorName][$column]);
 		}
 	}
 
